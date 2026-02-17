fb4836690393fe2c6d9feeb09c58b6974e29c19c
Rekurt-116 <sabrlitet@gmail.com>
2026-02-17 16:59:35 +0300

    loyaut home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,34 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { images } from '../../data/images-data';
+
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+public currentIndex: number = 0;
 
+images = images;
+
+// goTo(index: number) {
+//   this.currentIndex = index;
+// }
+
+next() {
+  if (this.images.length > 0) {
+    this.currentIndex = (this.currentIndex + 1) % this.images.length;
+  }
+}
+
+prev() {
+  if (this.images.length > 0) {
+    this.currentIndex =
+      (this.currentIndex - 1 + this.images.length) %
+      this.images.length;
+  }
+}
 }
