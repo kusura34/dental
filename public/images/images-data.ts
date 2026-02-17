64d634d45c89bf4bee8ebf2a2d10ad65dad15b00
Rekurt-116 <sabrlitet@gmail.com>
2026-02-17 11:19:45 +0300

    corection adaptation

---
+++ public/images/images-data.ts
@@ -0,0 +1,7 @@
+export const images = [
+    'https://static.tildacdn.com/tild3137-6663-4831-a166-643862313439/_.svg',
+    'https://static.tildacdn.com/tild6636-6631-4535-b839-353539636134/_.svg',
+    'https://static.tildacdn.com/tild3566-3130-4636-b963-646662666564/_.svg',
+    'https://static.tildacdn.com/tild3665-3936-4366-b865-386231613833/_.svg',
+    'https://static.tildacdn.com/tild3364-3861-4465-b363-373966633035/_.svg'
+] 
