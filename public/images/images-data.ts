3cb8b6ddda3c6b5d4d911ff9f9a5b512b0ccce53
Rekurt-116 <sabrlitet@gmail.com>
2026-02-24 15:43:27 +0300

    addaptation home page for tab&mob

--- public/images/images-data.ts
+++ public/images/images-data.ts
@@ -1,7 +1,18 @@
 export const images = [
-    'https://static.tildacdn.com/tild3137-6663-4831-a166-643862313439/_.svg',
-    'https://static.tildacdn.com/tild6636-6631-4535-b839-353539636134/_.svg',
-    'https://static.tildacdn.com/tild3566-3130-4636-b963-646662666564/_.svg',
-    'https://static.tildacdn.com/tild3665-3936-4366-b865-386231613833/_.svg',
-    'https://static.tildacdn.com/tild3364-3861-4465-b363-373966633035/_.svg'
-] 
+  'https://static.tildacdn.com/tild3137-6663-4831-a166-643862313439/_.svg',
+  'https://static.tildacdn.com/tild6636-6631-4535-b839-353539636134/_.svg',
+  'https://static.tildacdn.com/tild3566-3130-4636-b963-646662666564/_.svg',
+  'https://static.tildacdn.com/tild3665-3936-4366-b865-386231613833/_.svg',
+  'https://static.tildacdn.com/tild3364-3861-4465-b363-373966633035/_.svg',
+];
+
+export const safeImages: string[] = [
+  '/images/safe1.jpg',
+  '/images/safe2.jpg',
+  '/images/safe3.jpg',
+  '/images/safe4.jpg',
+  '/images/safe5.jpg',
+  '/images/safe6.jpg',
+  '/images/safe7.jpg',
+  '/images/safe8.jpg',
+];
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,22 +1,33 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { images } from '../../data/images-data';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { safeImages } from '../../../../public/images/images-data';
 
 
 @Component({
   selector: 'app-home',
   imports: [CommonModule],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
 public currentIndex: number = 0;
 
-images = images;
+showAll = false;
 
-// goTo(index: number) {
-//   this.currentIndex = index;
-// }
+images = images;
+safeImages = safeImages
+
+
+get visibleImages() {
+  return this.showAll ? this.safeImages : this.safeImages.slice(0, 2);
+}
+
+toggleImages() {
+  this.showAll = !this.showAll;
+}
 
 next() {
   if (this.images.length > 0) {
